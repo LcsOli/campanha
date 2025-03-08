1e293f9fe2e2c2f423a7c675ff13619fd8b2759b
LcsOli <lucasantonio97@live.com>
2025-03-08 11:54:02 -0300

    Feat: iniciando nova entity

---
+++ src/Vendas/entities/diario.entity.ts
@@ -0,0 +1,17 @@
+import { Entity, Column, PrimaryColumn } from 'typeorm';
+
+@Entity('diario')
+export class Venda {
+  @PrimaryColumn({ type: 'date' })
+  dtmov: string;
+
+  @PrimaryColumn({ type: 'varchar', length: 255 })
+  nome: string;
+
+  @Column({ type: 'int' })
+  pontos: number;
+
+  @Column({ type: 'decimal', precision: 10, scale: 2 })
+  total_vendido: number;
+
+}
