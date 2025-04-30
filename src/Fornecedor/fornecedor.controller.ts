2cf951fa34453e038377e78cd08b928e918464c1
lucasoli <lucasantonio97@live.com>
2025-04-30 16:05:02 -0300

    fix: alteração para filtrar por nome inves de cgc os itens

--- src/Fornecedor/fornecedor.controller.ts
+++ src/Fornecedor/fornecedor.controller.ts
@@ -1,16 +1,23 @@
 import { Controller, Get, Query } from "@nestjs/common";
 import { fornecedorService } from "./fornecedor.service";
+import { fornecedor } from "./entities/fornecedor.entity";
 
 @Controller('fornecedor')
 export class fornecedorController {
-    constructor(private readonly fornecedorService: fornecedorService) {}
+  constructor(private readonly fornecedorService: fornecedorService) {}
 
-    @Get()
-async getFornecedores(@Query("cgc") cgc?: string) {
+  @Get()
+  async getFornecedores(
+    @Query('nome') nome?: string,
+    @Query('cgc') cgc?: string,
+  ): Promise<fornecedor[]> {
+    if (nome) {
+      // Busca exata por nome; use QueryBuilder para LIKE se quiser busca parcial
+      return this.fornecedorService.findByNome(nome);
+    }
     if (cgc) {
-        return this.fornecedorService.findByCgc(Number(cgc)); 
+      return this.fornecedorService.findByCgc(Number(cgc));
     }
     return this.fornecedorService.findAll();
-}
-
+  }
 }
