a099169ff009e3a9e5f118acfb68cabb7b1a3b16
matheus alves <matheusalves0001@gmail.com>
2026-03-24 11:38:05 -0300

    Fix: Removido background service. Talvez eu va utiliza-lo no futuro para agendamento de pooling no banco.

--- src/Program.cs
+++ src/Program.cs
@@ -1,4 +1,3 @@
-using Campaign.API.Background;
 using Campaign.API.Configuration.Container_DI;
 using Campaign.API.Configuration.Container_DI.DataBaseContext;
 using Campaign.API.Configuration.ContainerDI.Handlers;
@@ -12,8 +11,6 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
-
-builder.Services.AddHostedService<TestBackgroundService>();
 
 builder.Services.AddHandlerInjection();
 builder.Services.AddIdentityInjection();
