e9777dcd58a5421c6bbe3e3d6230c6abb428b5ec
matheus alves <matheusalves0001@gmail.com>
2026-03-25 13:38:08 -0300

    feat: -Adicionado nova tabela para gerenciar os supervisores dos RCAs.

---
+++ shared/Campaign.Shared/DataBaseContext/Entities/Seller/SellerManager.cs
@@ -0,0 +1,15 @@
+﻿namespace Campaign.Shared.DataBaseContext.Entities.Seller
+{
+    public class SellerManager
+    {
+        public int Id { get; private set; }
+        public int Code { get; private set; }
+        public string Name { get; private set; } = default!;
+
+        public SellerManager(int code, string name)
+        {
+            Code = code;
+            Name = name;
+        }
+    }
+}
---
+++ shared/Campaign.Shared/DataBaseContext/EntitiesConfigurations/SellerManagerConfiguration.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Campaign.Shared.DataBaseContext.Entities.Seller;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Campaign.Shared.DataBaseContext.EntitiesConfigurations
+{
+    public class SellerManagerConfiguration : IEntityTypeConfiguration<SellerManager>
+    {
+        public void Configure(EntityTypeBuilder<SellerManager> builder)
+        {
+            builder.ToTable("CF_CAMPANHA_SUPERVISORES");
+
+            builder.HasKey(s => s.Id);
+
+            builder.Property(s => s.Id)
+                   .HasColumnName("ID");
+
+            builder.Property(s => s.Code)
+                   .HasColumnName("CODSUPERVISOR")
+                   .IsRequired();
+
+            builder.Property(s => s.Name)
+                   .HasColumnName("NOME")
+                   .IsRequired();
+
+            builder.HasData(new SellerManager(25, "Neto"),
+                            new SellerManager(3, "Bruno"),
+                            new SellerManager(11, "Bruno"),
+                            new SellerManager(12, "Bruno"),
+                            new SellerManager(13, "Bruno"),
+                            new SellerManager(33, "Bruno"),
+                            new SellerManager(27, "Rodrigo"),
+                            new SellerManager(30, "Leandro"),
+                            new SellerManager(34, "Leandro"),
+                            new SellerManager(14, "Zé Rubens"),
+                            new SellerManager(15, "Zé Rubens"),
+                            new SellerManager(28, "Zé Rubens"));
+        }
+    }
+}
