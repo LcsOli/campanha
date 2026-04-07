17413bf66c4d2d448c03e31d1a06ae81c64c706e
matheus alves <matheusalves0001@gmail.com>
2026-04-07 18:04:57 -0300

    fix: -Correcao de busca por clientes reativados; -Select foi alterado para retornar o vendedor e a quantidade de clientes que ele reativou no periodo.

--- src/Campaign.Pooling/Repositories/OrderSummary/ReadOnly/OrderSummaryReadOnlyRepository.cs
+++ src/Campaign.Pooling/Repositories/OrderSummary/ReadOnly/OrderSummaryReadOnlyRepository.cs
@@ -90,83 +90,81 @@ namespace Campaign.Pooling.Repositories.OrderSummary.ReadOnly
             dtWeekToStopProcess = dtWeekToStopProcess.Date;
 
             var query = _context.Database.SqlQuery<int>($@"
-                                                          SELECT
-                                                              c.codusur
+                                                          SELECT 
+                                                              codusur,
+                                                              COUNT(codusur) AS qtd_clientes_reativados
                                                           FROM
-                                                              pcpedc c
-                                                              JOIN pcusuari u on c.codusur = u.codusur
-                                                              JOIN pcpedi i on i.numped = c.numped
-                                                              JOIN pcpromoi p on p.codprod = i.codprod
-                                                              JOIN pcpromoc pc on pc.codpromocao = p.codpromocao
-                                                              JOIN pcclient client on c.codcli = client.codcli
-                                                              JOIN pcpromoc pcgeral on pcgeral.codpromocao = to_number(concat(to_char(pc.dtinicio, 'yyyy'), '00'))
-                                                          WHERE
-                                                              u.tipovend = 'R' AND
-                                                              c.codcli IN(
-                                                                        SELECT 
-                                                                            TO_NUMBER(REGEXP_SUBSTR({string.Join(",", consumersIds)}, '[^,]+', 1, LEVEL))
-                                                                        FROM 
-                                                                            dual
-                                                                        CONNECT BY REGEXP_SUBSTR({string.Join(",", consumersIds)}, '[^,]+', 1, LEVEL) IS NOT NULL
-                                                              ) AND
-
-
-
-                                                              p.codpromocao = {promotionCode} AND
-
-
-
-                                                              TO_CHAR(client.dtcadastro, 'yyyy-MM-DD') < CONCAT(TO_CHAR(pcgeral.dtinicio, 'yyyy'), '-01-01') AND
-
-
-                                                              (
-                                                                  TO_CHAR(c.data, 'yyyy-MM-DD') >= TO_CHAR(pcgeral.dtinicio, 'yyyy-MM-DD') AND
-                                                                  TO_CHAR(c.data, 'yyyy-MM-DD') <= TO_CHAR(pcgeral.dtfim, 'yyyy-MM-DD') 
-                                                              ) AND
-
-
-                                                              EXISTS(
-                                                                      SELECT 
-                                                                              1
-                                                                      FROM 
-                                                                          pcpedc cc 
-                                                                      WHERE 
-                                                                          cc.codcli = c.codcli AND
-                                                                          TO_CHAR(cc.data, 'yyyy-MM-DD') < CONCAT(TO_CHAR(pcgeral.dtinicio, 'yyyy'), '-01-01')
-                                                                      FETCH FIRST 1 ROW ONLY
-                                                              ) AND
-
-
-                                                              NOT EXISTS(
-                                                                          SELECT 
-                                                                              1
-                                                                          FROM 
+                                                          (
+                                                                SELECT
+                                                                    c.codusur,
+                                                                    c.codcli
+                                                                FROM
+                                                                    pcpedc c
+                                                                    JOIN pcusuari u on c.codusur = u.codusur
+                                                                    JOIN pcpedi i on i.numped = c.numped
+                                                                    JOIN pcpromoi p on p.codprod = i.codprod
+                                                                    JOIN pcpromoc pc on pc.codpromocao = p.codpromocao
+                                                                    JOIN pcclient client on c.codcli = client.codcli
+                                                                    JOIN pcpromoc pcgeral on pcgeral.codpromocao = to_number(concat(to_char(pc.dtinicio, 'yyyy'), '00'))
+                                                                WHERE
+                                                                    u.tipovend = 'R' AND
+                                                                    c.codcli IN(
+                                                                              SELECT 
+                                                                                  TO_NUMBER(REGEXP_SUBSTR({string.Join(",", consumersIds)}, '[^,]+', 1, LEVEL))
+                                                                              FROM 
+                                                                                  dual
+                                                                              CONNECT BY REGEXP_SUBSTR({string.Join(",", consumersIds)}, '[^,]+', 1, LEVEL) IS NOT NULL
+                                                                    ) AND
+                                                                    p.codpromocao = {consumersIds} AND
+                                                                    TO_CHAR(client.dtcadastro, 'yyyy-MM-DD') < CONCAT(TO_CHAR(pcgeral.dtinicio, 'yyyy'), '-01-01') AND
+                                                                    (
+                                                                        TO_CHAR(c.data, 'yyyy-MM-DD') >= TO_CHAR(pcgeral.dtinicio, 'yyyy-MM-DD') AND
+                                                                        TO_CHAR(c.data, 'yyyy-MM-DD') <= TO_CHAR(pcgeral.dtfim, 'yyyy-MM-DD') 
+                                                                    ) AND
+                                                                    EXISTS(
+                                                                            SELECT 
+                                                                                    1
+                                                                            FROM 
+                                                                                pcpedc cc 
+                                                                            WHERE 
+                                                                                cc.codcli = c.codcli AND
+                                                                                TO_CHAR(cc.data, 'yyyy-MM-DD') < CONCAT(TO_CHAR(pcgeral.dtinicio, 'yyyy'), '-01-01')
+                                                                            FETCH FIRST 1 ROW ONLY
+                                                                    ) AND
+                                                                    NOT EXISTS(
+                                                                                SELECT 
+                                                                                    1
+                                                                                FROM 
+                                                                                    pcpedc cc
+                                                                                WHERE 
+                                                                                    cc.codcli = c.codcli AND
+                                                                                    (
+                                                                                        TO_CHAR(cc.data, 'YYYY-MM-DD') >= concat(TO_CHAR(pc.dtinicio, 'yyyy'), '-01-01') AND
+                                                                                        TO_CHAR(cc.data, 'YYYY-MM-DD') < TO_CHAR(pcgeral.dtinicio, 'yyyy-MM-DD')
+                                                                                    )
+                                                                                FETCH FIRST 1 ROW ONLY
+                                                                    ) AND
+                                                                    EXISTS(
+                                                                          SELECT
+                                                                              cc.codcli
+                                                                          FROM
                                                                               pcpedc cc
-                                                                          WHERE 
+                                                                          WHERE
                                                                               cc.codcli = c.codcli AND
-                                                                              (
-                                                                                  TO_CHAR(cc.data, 'YYYY-MM-DD') >= concat(TO_CHAR(pc.dtinicio, 'yyyy'), '-01-01') AND
-                                                                                  TO_CHAR(cc.data, 'YYYY-MM-DD') < TO_CHAR(pcgeral.dtinicio, 'yyyy-MM-DD')
-                                                                              )
-                                                                          FETCH FIRST 1 ROW ONLY
-                                                              ) AND
-                                                              EXISTS(
-                                                                    SELECT
-                                                                        cc.codcli
-                                                                    FROM
-                                                                        pcpedc cc
-                                                                    WHERE
-                                                                        cc.codcli = c.codcli AND
-                                                                        TO_CHAR(cc.data, 'yyyy-MM-DD') >= TO_CHAR(pcgeral.dtinicio, 'yyyy-MM-DD') AND
-                                                                        TO_CHAR(cc.data, 'yyyy-MM-DD') <= TO_CHAR(pcgeral.dtfim, 'yyyy-MM-DD') 
-                                                                    GROUP BY
-                                                                        cc.codcli
-                                                                    having
-                                                                        TO_CHAR(min(cc.data), 'yyyy-MM-DD') >= TO_CHAR(pc.dtinicio, 'yyyy-MM-DD') AND
-                                                                        TO_CHAR(min(cc.data), 'yyyy-MM-DD') <= TO_CHAR(pc.dtfim, 'yyyy-MM-DD')
-                                                              )
-                                                          GROUP BY 
-                                                              c.codusur;
+                                                                              TO_CHAR(cc.data, 'yyyy-MM-DD') >= TO_CHAR(pcgeral.dtinicio, 'yyyy-MM-DD') AND
+                                                                              TO_CHAR(cc.data, 'yyyy-MM-DD') <= TO_CHAR(pcgeral.dtfim, 'yyyy-MM-DD') 
+                                                                          GROUP BY
+                                                                              cc.codcli
+                                                                          having
+                                                                              TO_CHAR(min(cc.data), 'yyyy-MM-DD') >= to_char(pc.dtinicio, 'yyyy-MM-DD') AND
+                                                                              TO_CHAR(min(cc.data), 'yyyy-MM-DD') <= to_char(pc.dtfim, 'yyyy-MM-DD')
+                                                                    )
+                                                                GROUP BY 
+                                                                    c.codusur,
+                                                                    c.codcli
+                                                          )
+                                                         GROUP BY 
+                                                             codusur
             ");
 
             return await query.ToArrayAsync();
