26d3f90d436f1ccf6289be012d9e84690bce44a3
matheus alves <matheusalves0001@gmail.com>
2026-05-26 17:13:51 -0300

    feat -Cors configurado

--- shared/Campaign.Shared/Cors/Cors.cs
+++ shared/Campaign.Shared/Cors/Cors.cs
@@ -6,10 +6,12 @@ namespace Campaign.Shared.cors
     {
         public static void AddCorsConfiguration(this IServiceCollection services)
         {
-            services.AddCors(options =>            {
+            services.AddCors(options =>
+            {
                 options.AddPolicy("cors", policy =>
                 {
-                    policy.WithOrigins("http://localhost:8082")
+                    policy.WithOrigins("http://localhost:8084",
+                                       "http://192.168.10.9:8084")
                           .AllowAnyHeader()
                           .AllowAnyMethod();
                 });
