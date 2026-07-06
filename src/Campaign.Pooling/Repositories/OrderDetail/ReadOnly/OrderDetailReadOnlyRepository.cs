8ca07c6417b786eca862212cbcdfc1f75a5ce249
matheus alves <matheusalves0001@gmail.com>
2026-07-06 08:58:59 -0300

    feat -  Ajustado processador de pontos.

--- src/Campaign.Pooling/Repositories/OrderDetail/ReadOnly/OrderDetailReadOnlyRepository.cs
+++ src/Campaign.Pooling/Repositories/OrderDetail/ReadOnly/OrderDetailReadOnlyRepository.cs
@@ -56,11 +56,6 @@ namespace Campaign.Pooling.Repositories.OrderDetail.ReadOnly
                                 JOIN pcclient c ON c.codcli = pc.codcli
                                 JOIN pcprodut p ON p.codprod = pi.codprod
                             WHERE
-
-                                s.rca_id = 522 AND
-
-
-
                                 pc.dtcancel IS NULL AND
                                 pm.codpromocao = {promotionCode} AND
                                 (
